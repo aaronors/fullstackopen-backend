dbbdf482985c8febb0e5a26a98f6d0a7dd7f7405
Aaron Salazar <aaronors@outlook.com>
2022-12-16 20:15:00 -0800

    part3 - ex3.4 - implement delete

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -45,12 +45,12 @@ app.get("/api/persons/:id", (request, response) => {
     }
 });
 
-// app.delete("/api/persons/:id", (request, response) => {
-//     const id = Number(request.params.id);
-//     persons = persons.filter((person) => person.id !== id);
+app.delete("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id);
+    persons = persons.filter((person) => person.id !== id);
 
-//     response.status(204).end();
-// });
+    response.status(204).end();
+});
 
 // const generateId = () => {
 //     const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
