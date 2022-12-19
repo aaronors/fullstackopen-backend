d1bbad2177b72325167ac4b0f277c393612cc80e
Aaron Salazar <aaronors@outlook.com>
2022-12-18 20:39:31 -0800

    part3 - ex3.9-3.11 - build and deploy front/back end to fly

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const morgan = require('morgan')
 
 const app = express();
+const cors = require('cors');
 app.use(express.json());
+app.use(cors());
 app.use(morgan("tiny"));
+app.use(express.static('build'));
 
 let persons = [
     { 
@@ -85,7 +88,7 @@ app.post("/api/persons", (request, response) => {
     response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
