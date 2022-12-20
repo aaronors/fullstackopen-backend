5e9c0e4d110af653fb50be7471f60baf733e96e8
Aaron Salazar <aaronors@outlook.com>
2022-12-19 20:44:52 -0800

    part3 - ex3.13 - update fetch all numbers from mongo

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require('morgan')
+const Person = require("./models/person");
 
 const app = express();
 const cors = require('cors');
@@ -8,35 +10,14 @@ app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.static('build'));
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-];
-
 app.get("/info", (request, response) => {
     response.send(`<div>Phonebook has info for ${persons.length} people</div><div>${new Date()}</div>`);
 });
 
 app.get("/api/persons", (request, response) => {
-    response.json(persons);
+    Person.find({}).then((persons) => {
+        response.json(persons);
+    });
 });
 
 app.get("/api/persons/:id", (request, response) => {
