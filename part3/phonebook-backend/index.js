78d1b65f0c41256a84602a97763914f82cac145a
Aaron Salazar <aaronors@outlook.com>
2022-12-20 14:57:13 -0800

    part3 - ex3.19 - name validation

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -74,7 +74,7 @@ app.delete("/api/persons/:id", (request, response) => {
         .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
     const body = request.body;
 
     if (!body.name || !body.number) {
@@ -88,9 +88,11 @@ app.post("/api/persons", (request, response) => {
         number: body.number,
     });
 
-    person.save().then((savedPerson) => {
-        response.json(savedPerson);
-    });
+    person.save()
+        .then((savedPerson) => {
+            response.json(savedPerson);
+        })
+        .catch(error => next(error))
 });
 
 const errorHandler = (error, request, response, next) => {
@@ -98,6 +100,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === "CastError") {
         return response.status(400).send({ error: "malformatted id" });
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error);
